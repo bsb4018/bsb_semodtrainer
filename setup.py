c1800d1901e8eb6bb6c39d177addd4be47b8229b
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-12-15 16:36:06 +0530

    Bug Fix

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shibendra Bhattacharjee",
+    author_email="storebshiv@gmail.com",
+    packages=find_packages()
+)
